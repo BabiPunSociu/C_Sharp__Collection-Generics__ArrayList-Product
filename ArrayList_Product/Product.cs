14c0aef03dfdbae9c38e77ce431be2cbbcfcc2f0
BabiPunSociu <Nguyendung281002@gmail.com>
2023-06-07 15:04:58 +0700

    DungNguyen

---
+++ ArrayList_Product/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Product
+{
+    internal class Product
+    {
+        string name;    // tên sản phẩm
+        double price;   // Giá sản phẩm
+        int onhand;     // Số lượng sản phẩm sẵn có
+
+        public Product(string name, double price, int onhand)
+        {
+            this.name = name;
+            this.price = price;
+            this.onhand = onhand;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-10} Price: {1,6:C} On hand: {2}",name, price, onhand);
+        }
+
+    }
+}
---
+++ ArrayList_Product/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;           // class ArrayList được chứa trong namespace "System.Collections"
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayList_Product
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Tạo ArrayList
+            ArrayList sanPham = new ArrayList();
+
+            // Thêm giá trị vào mảng
+            sanPham.Add(new Product("A", 5.9, 3));
+            sanPham.Add(new Product("B", 8.2, 2));
+            sanPham.Add(new Product("C", 3.5, 4));
+            sanPham.Add(new Product("D", 1.8, 8));
+
+            // in danh sách
+
+            foreach(Product sp in sanPham)
+            {
+                Console.WriteLine(" " + sp);
+            }    
+        }
+    }
+}
